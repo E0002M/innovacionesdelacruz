1aa1c907862f6fb4270c1757ce14522db5a1d870
E0002M <81307241+E0002M@users.noreply.github.com>
2025-01-19 18:40:28 -0600

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,33 +5,44 @@ const config: Config = {
   content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      // Fuentes clásicas para un estilo vintage
       fontFamily: {
-        sans: ['var(--font-geist-sans)']
+        sans: ['Roboto', 'sans-serif'], // Fuente para el cuerpo del texto
+        serif: ['Playfair Display', 'serif'], // Fuente para títulos
+      },
+      // Paleta de colores vintage
+      colors: {
+        primary: '#8B4513', // Marrón cálido para botones y encabezados
+        secondary: '#D2B48C', // Beige claro para fondos o acentos
+        accent: '#A0522D', // Marrón rojizo para detalles pequeños
+        background: '#F5F5DC', // Beige claro para el fondo principal
+        text: '#5C4033', // Marrón oscuro para textos
+        highlight: '#FFE4C4', // Beige cremoso para destacar elementos
       },
       keyframes: {
         fadeIn: {
           from: { opacity: '0' },
-          to: { opacity: '1' }
+          to: { opacity: '1' },
         },
         marquee: {
           '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(-100%)' }
+          '100%': { transform: 'translateX(-100%)' },
         },
         blink: {
           '0%': { opacity: '0.2' },
           '20%': { opacity: '1' },
-          '100%': { opacity: '0.2' }
-        }
+          '100%': { opacity: '0.2' },
+        },
       },
       animation: {
         fadeIn: 'fadeIn .3s ease-in-out',
         carousel: 'marquee 60s linear infinite',
-        blink: 'blink 1.4s both infinite'
-      }
-    }
+        blink: 'blink 1.4s both infinite',
+      },
+    },
   },
   future: {
-    hoverOnlyWhenSupported: true
+    hoverOnlyWhenSupported: true,
   },
   plugins: [
     require('@tailwindcss/container-queries'),
@@ -41,16 +52,16 @@ const config: Config = {
         {
           'animation-delay': (value) => {
             return {
-              'animation-delay': value
+              'animation-delay': value,
             };
-          }
+          },
         },
         {
-          values: theme('transitionDelay')
+          values: theme('transitionDelay'),
         }
       );
-    })
-  ]
+    }),
+  ],
 };
 
 export default config;
